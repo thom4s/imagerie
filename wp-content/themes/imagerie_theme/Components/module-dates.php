56e5702e17b0f8fef8c17dbeca80914423cd0280
thomas <hello@thomasflorentin.net>
2025-03-21 17:26:51 +0100

    polish

--- wp-content/themes/imagerie_theme/Components/module-dates.php
+++ wp-content/themes/imagerie_theme/Components/module-dates.php
@@ -12,12 +12,31 @@ if( $dates ) :
         $descr = $d['chrono_description']; ?>
 
 
-        <?php echo $date; ?>
-        <img src="<?php echo $photo; ?>" alt="">
-        <?php echo $legend; ?>
-        <?php echo $title; ?>
-        <?php echo $descr; ?>
+    <section class="module2">
 
+        <img class="imgchrono1" src="<?php echo $photo; ?>">
+        <?php echo $legend; ?>
+
+        <div class="timeline">
+            <p class="textdate"><?php echo $date; ?></p>
+            <hr width="1px" size="75px" />
+            <p class="textdate">1987</p>
+            <hr width="1px" size="75px" />
+            <p class="textdate">2012</p>
+            <hr width="1px" size="75px" />
+            <p class="textdate">2018</p>
+            <hr width="1px" size="75px" />
+            <p class= "textdate">2027</p>
+        </div>
+
+        <div class ="containerstep">
+            <div class= "steptitle"><?php echo $title; ?></div>
+            <div class= "step-content">
+                <?php echo $descr; ?>
+            </div>
+        </div>
+
+    </section>
         
     <?php endforeach; ?>
 <?php endif; ?>
--- wp-content/themes/imagerie_theme/Components/module-push.php
+++ wp-content/themes/imagerie_theme/Components/module-push.php
@@ -1,21 +1,32 @@
 <?php
-$projects = $args['projects'];
-var_dump($projects);
+$projects = $args['projects']; ?>
 
 
-if( $projects ) :
-    foreach( $projects as $p ) :
+<section class="pushprojects">
+    <div class="containerframe">
 
-        var_dump($p);
+        <?php if( $projects ) :
+            foreach( $projects as $p ) :
+                $id = $p->ID;
+                $title = $p->post_title;
+                $thumb = get_the_post_thumbnail($id, 'medium');
+                $subtitle = get_field('', $id);
 
-        $title = $p['post_title'];
-        $content = $p['post_content'];
-    ?>
+            ?>
 
 
-        <?php echo $title; ?>
-        <?php echo $content; ?>
+                <div class="containerproj">
+                    <a href="<?php the_permalink($id); ?>">
+                        <?php echo $thumb; ?>
+                        <h2><?php echo $title; ?></h2>
+                        <p class="textsubtitle">Le sous-titre</p>
+                        <p>INFORMATIONS COMPLÉMENTAIRES</p>
+                    </a>
+                </div>
+                
+                
+            <?php endforeach; ?>
+        <?php endif; ?>
 
-        
-    <?php endforeach; ?>
-<?php endif; ?>
+    </div>
+</section>
