2b76579d81ee91991f3a0351831464b1c32b1f45
Thomas <hello@thomasflorentin.net>
2026-02-13 15:44:14 +0100

    templates

---
+++ wp-content/themes/imagerie_theme/page-2025-keywords.php
@@ -0,0 +1,41 @@
+<?php 
+    /* template Name: Mots clés Promotion 2025 */
+get_header(); ?>
+
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                <h1><?php the_title(); ?></h1>
+                <div class="wysiwyg">
+                    <?php the_content(); ?>
+                </div>
+
+
+
+    <?php if( have_rows('modules') ):
+
+        // Loop through rows.
+        while ( have_rows('modules') ) : the_row();
+
+            if( get_row_layout() == 'module_hero' ):
+
+                get_template_part('Components/module', 'hero', array(
+                    'title' =>  get_sub_field('module_title'),
+                    'consigne' => get_sub_field('consigne_scroll'),
+                    'image' => get_sub_field('module_background')
+                )); 
+
+
+            endif;
+
+        // End loop.
+        endwhile;
+
+    endif;
+    ?>
+
+
+
+<?php endwhile;
+endif; ?>
+<?php get_footer(); ?>
