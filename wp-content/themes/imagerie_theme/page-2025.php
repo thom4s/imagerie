750314b5a9c0e0fe9d9e80c5b806e1b0c5bfc0c0
Thomas <hello@thomasflorentin.net>
2026-02-16 09:59:14 +0100

    Update page-2025.php

--- wp-content/themes/imagerie_theme/page-2025.php
+++ wp-content/themes/imagerie_theme/page-2025.php
@@ -17,12 +17,23 @@ get_header(); ?>
         // Loop through rows.
         while ( have_rows('modules') ) : the_row();
 
-            if( get_row_layout() == 'module_hero' ):
+            if( get_row_layout() == 'cover' ):
 
                 get_template_part('Components/module', 'hero', array(
-                    'title' =>  get_sub_field('module_title'),
-                    'consigne' => get_sub_field('consigne_scroll'),
-                    'image' => get_sub_field('module_background')
+                    'image' =>  get_sub_field('background_IMA'),
+                    'title' => get_sub_field('cover_title'),
+                )); 
+
+            elseif( get_row_layout() == 'module1' ):
+
+                get_template_part('Components/module', 'hero', array(
+                    'title' =>  get_sub_field('module1_title'),
+                    'content' => get_sub_field('module1_content'),
+                    'image' => get_sub_field('module1_img'),
+                    'bg_color' => get_sub_field('module1_bg'),
+                    'inversed' => get_sub_field('module1_inversed'),
+                    'btn_label' => get_sub_field('module1_btn_label'),
+                    'btn_url' => get_sub_field('module1_btn_url'),
                 )); 
 
 
