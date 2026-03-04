f12c35d290610b063a8ae111cc4176e61346c76f
Thomas <hello@thomasflorentin.net>
2026-03-04 16:36:07 +0100

    Update module-un.php

--- wp-content/themes/imagerie_theme/Components/2025/module-un.php
+++ wp-content/themes/imagerie_theme/Components/2025/module-un.php
@@ -3,25 +3,27 @@
 <section class="je_bloc_paragraphe_accueil" style="background-color: <?php echo $args['bg_color']; ?>">
     <div class="wrapper">
 
-      <div class="columns">
+      <div class="columns <?php echo $args['inversed'][0]; ?>">
 
         <div>
-            <h3 class="je_titre_generique"><?php echo $args['title']; ?></h3>
-            <h3 class="je_soustitre_generique">
-                Retrouvez les notions clés du projet
-            </h3>
-        </div>
-        
-        <div>
-            <p class="je_typo_paragraphe">
-                <?php echo $args['content']; ?>
-            </p>
+            <div>
+                <h3 class="je_titre_generique"><?php echo $args['title']; ?></h3>
+                <h3 class="je_soustitre_generique">
+                    Retrouvez les notions clés du projet
+                </h3>
+            </div>
+            
+            <div>
+                <p class="je_typo_paragraphe">
+                    <?php echo $args['content']; ?>
+                </p>
 
-            <a href="<?php echo $args['btn_url']; ?>" class="je_bouton_decouvrir">
-                <img class="je_iconeleft" src="flèche.png" />
-                <span><?php echo $args['btn_label']; ?></span>
-            </a>
-                
+                <a href="<?php echo $args['btn_url']; ?>" class="je_bouton_decouvrir">
+                    <img class="je_iconeleft" src="flèche.png" />
+                    <span><?php echo $args['btn_label']; ?></span>
+                </a>
+                    
+            </div>
         </div>
 
         <div class="je_presentation_nuage_image">
@@ -33,5 +35,5 @@
 </section>	
 
 
-<?php //echo $args['inversed'][0]; ?>
+
 
