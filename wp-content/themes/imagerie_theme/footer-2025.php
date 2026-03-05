858b9c368e527e161be6f798dd6b05a66647f620
Thomas <hello@thomasflorentin.net>
2026-03-05 10:00:14 +0100

    Update footer-2025.php

--- wp-content/themes/imagerie_theme/footer-2025.php
+++ wp-content/themes/imagerie_theme/footer-2025.php
@@ -4,17 +4,17 @@
 
 	<footer>
 		<section class="je_bloc_footer_accueil">
-			<div class=je_bloc_footer_accueil>
+			<div class="wrapper">
+				<div>
+					<img src="<?php echo get_template_directory(); ?>/img/vingtcinq/logo.png" alt="">
+				</div>
 
-			<div>
-				<img src="<?php echo get_template_directory(); ?>/img/vingtcinq/logo.png" alt="">
-			</div>
-
-			<div class="je_footer-liens">
-				<a href="contact.html">Contact</a>
-				<a href="reglement.html">Règlement de visite</a>
-				<a href="mentions-legales.html">Mentions légales</a>
-				<a href="confidentialite.html">Politique de confidentialité</a>
+				<div class="je_footer-liens">
+					<a href="contact.html">Contact</a>
+					<a href="reglement.html">Règlement de visite</a>
+					<a href="mentions-legales.html">Mentions légales</a>
+					<a href="confidentialite.html">Politique de confidentialité</a>
+					</div>
 				</div>
 			</div>
 		</section>
