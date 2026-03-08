56d4b90d15630bb62300568979fc816f6b9d81bb
Thomas <hello@thomasflorentin.net>
2026-03-08 17:01:17 +0100

    polish

--- wp-content/themes/imagerie_theme/Components/2025/module-deux.php
+++ wp-content/themes/imagerie_theme/Components/2025/module-deux.php
@@ -10,18 +10,18 @@
 
         <div>
             <h3><?php echo $args['title']; ?></h3>
-            <p><?php echo $args['content']; ?></p>
+            <div class="je_typo_paragraphe"><?php echo $args['content']; ?></div>
         </div>
  
         <div class="je_bloc_portrait_accueil">
-            <div class="columns">
+            <div class="columns --free">
                 <?php foreach( $repeater as $r ) :
                     $image = $r['module2_vignette'];
                     $legend = $r['module2_legend']; ?>
         
                 <div>
                     <img class="je_Bondil" src="<?php echo $image; ?>"></img>
-                    <p><?php echo $legend; ?></p>
+                    <div><?php echo $legend; ?></div>
                 </div>
 
                 <?php endforeach; ?>
--- wp-content/themes/imagerie_theme/Projects/project-2025.php
+++ wp-content/themes/imagerie_theme/Projects/project-2025.php
@@ -2,4 +2,3 @@
     wp_enqueue_script('nuage-script');
     wp_enqueue_style('nuage-style'); 
 ?>
-<p>nuage</p>
